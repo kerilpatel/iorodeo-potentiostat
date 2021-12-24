8fca08c94a035d7387dd455282a69cab352dda9f
Naachiket Pant <naachiketpant88@gmail.com>
2021-12-24 18:26:24 +0530

    Have different tabs now

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
+import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
-root = Tk()
 
-#Defining the window
-root.geometry("1280x720")
+#creating parent window
+root = tk.Tk()
 root.title("IORODEO POTENTIOSTAT")
 
-#The menu bar (Inside root)
-menubar = Menu(root)
+#adding tabs to the parent window
+tabcontrol = ttk.Notebook(root)
 
-#Options (Inside menu bar)
-options = Menu(menubar, tearoff = 0)
-options.add_command(label = "Device Connection")
-options.add_command(label = "Test and Parameters")
-options.add_command(label = "Data Acquisition")
+#creating tabs
+tab1 = ttk.Frame(tabcontrol)
+tab2 = ttk.Frame(tabcontrol)
+tab3 = ttk.Frame(tabcontrol)
 
-options.add_separator()
+#adding tab
+tabcontrol.add(tab1, text = "Device Connection")
+tabcontrol.add(tab2, text = "Test and Parameters")
+tabcontrol.add(tab3, text = "Data Acquisition")
 
-#menubar.add_cascade(label = "Options", menu = options)
+tabcontrol.pack(expand = 1, fill = "both")
 
-root.config(menu = options)
+#creating example labels inside the tabs
+ttk.Label(tab1, text="This is the Device Connection tab").grid(column = 0, row = 0, padx = 360, pady = 360)
+ttk.Label(tab2, text = "This is the test and parameters tab").grid(column = 0, row = 0, padx = 360, pady = 360)
+ttk.Label(tab3, text = "This is the data acquisition tab").grid(column = 0, row = 0, padx = 360, pady = 360)
 
 root.mainloop()
-
